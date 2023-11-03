727eb17cd7923ab0858281a33bdb6e6b9067371f
RedYUkno <90758538+hf1037277956@users.noreply.github.com>
2023-11-03 17:54:14 +0800

    添加AB包管理工具

---
+++ Assets/Scripts/Base/Res/AssetBundleManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleManager : MonoSingleton<AssetBundleManager>
+{
+    // AB包缓存  解决AB包无法重复加载的问题 也有利于提高效率
+    private Dictionary<string, AssetBundle> _abCache;
+
+    // 主包
+    private AssetBundle _mainAB = null; 
+    
+    // 主包中配置文件，用来获取依赖包
+    private AssetBundleManifest _mainManifest = null;
+
+    private string basePath
+    {
+        get
+        {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+            return Application.dataPath + "/StreamingAssets/";
+#elif UNITY_ANDROID
+            return Application.dataPath + "!/assets/";
+#endif   
+        }
+    }
+
+    private string mainABName
+    {
+        get
+        {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+            return "StandaloneWindows";
+#elif UNITY_ANDROID
+            return "Android";
+#endif
+        }
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+        
+        _abCache = new Dictionary<string, AssetBundle>();
+    }
+
+    
+}
---
+++ Assets/Scripts/MonoBehaviour/MonoSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T:MonoSingleton<T>
+{
+    private static T _instance;
+    
+    public static T Instance
+    {
+        get
+        {
+            if (_instance != null) return _instance;
+            
+            _instance = FindObjectOfType<T>();
+            
+            if (_instance == null)
+            {
+                new GameObject("Singleton of " + typeof(T)).AddComponent<T>();
+            }
+            else
+            {
+                _instance.Init();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this as T;
+        Init();
+    }
+
+    protected virtual void Init()
+    {
+        
+    }
+}
