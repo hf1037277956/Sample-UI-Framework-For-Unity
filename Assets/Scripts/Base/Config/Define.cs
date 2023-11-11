14c051f3d75e6cd639fbdb93df5a77794f135efb
RedYUkno <swzclo@qq.com>
2023-11-11 16:46:27 +0800

    导入Addresable包，添加自动设置组路径的逻辑

---
+++ Assets/Scripts/Base/Config/Define.cs
@@ -0,0 +1,35 @@
+﻿public static class Define
+{
+    #region 路径
+
+    public const string AudioPath = "Audio/";
+    public const string SpinePath = "Spines/";
+    public const string TexturePath = "Textures/";
+    public const string AtlasPath = "UIAtlas/";
+    public const string PrefabPath = "Prefabs/";
+    public const string UIPrefabPath = "UI/";
+
+    #endregion
+
+    #region 后缀名
+
+    public const string DotPrefab = ".prefab";
+    public const string DotPng = ".png";
+    public const string DotSpriteAtlas = ".spriteatlas";
+    public const string DotAsset = ".asset";
+    public const string DotWav = ".wav";
+
+    #endregion
+
+    #region UI
+    
+    public const string GUI = "GUI";
+
+    #endregion
+
+    #region Addressables
+
+    public const string Label_default = "default";
+
+    #endregion
+}
---
+++ Assets/Scripts/Editor/AssetPostProcessor/AddressableProcessor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEditor;
+using UnityEditor.AddressableAssets.Settings;
+
+public class AddressableProcessor : AssetPostprocessor
+{
+    public const string prefix = "Assets/Res/";
+
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        bool isDirty = false;
+        
+        foreach (var str in importedAssets)
+        {
+            if (IsAddressable(str, out string address, out string groupName, out string labelName))
+            {
+                var setting = AssetDatabase.LoadAssetAtPath<AddressableAssetSettings>("Assets/AddressableAssetsData/AddressableAssetSettings.asset");
+                var group = setting.FindGroup(groupName);
+                var label = labelName;
+
+                var guid = AssetDatabase.AssetPathToGUID(str);
+                var entry = setting.CreateOrMoveEntry(guid, group);
+                entry.SetAddress(address);
+                entry.SetLabel(label, true);
+                
+                isDirty = true;
+            }
+        }
+        
+        if (isDirty)
+        {
+            AssetDatabase.SaveAssets();
+        }
+    }
+
+    private static bool IsAddressable(string asset, out string address, out string groupName, out string labelName)
+    {
+        if (asset.StartsWith(prefix + Define.UIPrefabPath) && asset.EndsWith(Define.DotPrefab))
+        {
+            address = asset.Substring(prefix.Length);
+            groupName = "Prefabs";
+            labelName = Define.Label_default;
+            return true;
+        }
+        if (asset.StartsWith(prefix + Define.SpinePath) && asset.EndsWith("SkeletonData" + Define.DotAsset))
+        {
+            address = asset.Substring(prefix.Length);
+            groupName = "Spines";
+            labelName = Define.Label_default;
+            return true;
+        }
+        if (asset.StartsWith(prefix + Define.TexturePath) && asset.EndsWith(Define.DotPng))
+        {
+            address = asset.Substring(prefix.Length);
+            groupName = "Textures";
+            labelName = Define.Label_default;
+            return true;
+        }
+        if (asset.StartsWith(prefix + Define.AtlasPath) && asset.EndsWith(Define.DotSpriteAtlas))
+        {
+            address = asset.Substring(prefix.Length);
+            groupName = "UIAtlas";
+            labelName = Define.Label_default;
+            return true;
+        }
+        if (asset.StartsWith(prefix + Define.AudioPath) && asset.EndsWith(Define.DotWav))
+        {
+            address = asset.Substring(prefix.Length);
+            groupName = "Audio";
+            labelName = Define.Label_default;
+            return true;
+        }
+
+
+        address = null;
+        groupName = null;
+        labelName = null;
+        return false;
+    }
+}
